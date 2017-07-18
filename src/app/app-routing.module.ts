c5b3eb05f4ae40f8ff1efc7807e3b3e6929257ae
goze <goze.qiu@gmail.com>
2017-07-18 17:43:50 +0800

    feat: add demodetail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DemolistComponent } from './demolist/demolist.component';
+import { DemodetailComponent } from './demodetail/demodetail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard',  component: DashboardComponent },
-  { path: 'demolist',  component: DemolistComponent }
+  { path: 'demolist',  component: DemolistComponent },
+  { path: 'demodetail/:id',  component: DemodetailComponent },
 ];
 
 @NgModule({
