9fd7276eeb19970289a60644be26e885c1be979e
goze <goze.qiu@gmail.com>
2017-07-19 14:10:20 +0800

    feat: editor set input && readonly

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import * as ace from 'brace';
+import { Editor } from 'brace';
+import 'brace/theme/xcode.js';
+import 'brace/mode/javascript.js';
 
 @Component({
   selector: 'app-editor',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editor.component.css']
 })
 export class EditorComponent implements OnInit {
+  private editor: Editor;
+  @Input() code: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.editor = ace.edit('editor');
+    this.editor.setOptions({
+      tabSize: 2
+    });
+    this.editor.setTheme('ace/theme/xcode');
+    this.editor.getSession().setMode('ace/mode/javascript');
+    this.editor.setValue(this.code);
+    this.editor.setReadOnly(true);
   }
-
 }
