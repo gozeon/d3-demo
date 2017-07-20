a387f4c047c2e396b70af521818c423919baf1a8
goze <goze.qiu@gmail.com>
2017-07-20 15:38:26 +0800

    fix: templateUrl && styleUrls

--- src/app/demos/makingbarchart/makingbarchart.component.ts
+++ src/app/demos/makingbarchart/makingbarchart.component.ts
@@ -3,8 +3,8 @@ import * as d3 from 'd3';
 
 @Component({
   selector: 'app-makingbarchart',
-  templateUrl: '../common/common.html',
-  styleUrls: ['../common/common.css']
+  templateUrl: '../../common/common.html',
+  styleUrls: ['../../common/common.css']
 })
 export class MakingbarchartComponent implements OnInit {
   private code: string;
